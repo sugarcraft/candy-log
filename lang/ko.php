51cd0d099839ede4ac84991944675636f372305b
Joe Huss <detain@interserver.net>
2026-05-07 05:56:41 -0400

    Add Korean locale for all libs (#257)

---
+++ lang/ko.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Korean translations for candy-log.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'logger.fatal' => '치명적 로그: {message}',
+];
